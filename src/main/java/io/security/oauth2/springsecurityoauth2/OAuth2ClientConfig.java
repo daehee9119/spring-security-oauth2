373d4f03967cf2279a4d4073b5a2a36b45007589
daehee.kim <daehee.kim.extern@allane.com>
2024-12-09 23:41:44 +0900

    authorizedclientmanager'

--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
@@ -1,32 +1,24 @@
 package io.security.oauth2.springsecurityoauth2;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class OAuth2ClientConfig {
 
-    @Autowired
-    private ClientRegistrationRepository clientRegistrationRepository;
 
     @Bean
     SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests((requests) -> requests.antMatchers("/home").permitAll()
-                .anyRequest().authenticated());
-        http.oauth2Login(authLogin ->
-                authLogin.authorizationEndpoint(authEndpoint ->
-                        authEndpoint.authorizationRequestResolver(customOAuth2AuthenticationRequestResolver())));
+        http
+                .authorizeRequests(requests -> requests.antMatchers("/home", "/client", "/logout").permitAll()
+                        .anyRequest().authenticated())
+                //                .oauth2Login(Customizer.withDefaults())
+                .oauth2Client(Customizer.withDefaults());
         http.logout().logoutSuccessUrl("/home");
         return http.build();
    }
 
-    private OAuth2AuthorizationRequestResolver customOAuth2AuthenticationRequestResolver() {
-        return new CustomOAuth2AuthorizationRequestResolver(clientRegistrationRepository, "/oauth2/authorization");
-    }
 }
